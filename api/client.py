da17d1148d7237042d2252df378d7c224700978c
Ariel Martínez Silberstein <amartinez5@uc.cl>
2021-04-04 19:22:11 -0400

    fix: update an endpoint

--- api/client.py
+++ api/client.py
@@ -1,4 +1,4 @@
 from .utils import api_request
 
-choose_result = lambda: api_request(method="PATCH", path="/draws/ongoing")
+choose_result = lambda: api_request(method="POST", path="/draws/ongoing/choose")
 create_draw = lambda: api_request(method="POST", path="/draws")
